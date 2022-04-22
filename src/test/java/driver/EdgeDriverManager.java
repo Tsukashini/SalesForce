7d05bec39c02d9e6ac7c1edf133fac183f3fff42
Tsukashini <veretig@yandex.by>
2022-04-22 17:54:30 +0300

    init commit

---
+++ src/test/java/driver/EdgeDriverManager.java
@@ -0,0 +1,11 @@
+package driver;
+
+import java.net.MalformedURLException;
+
+public class EdgeDriverManager extends DriverManager{
+
+    @Override
+    public void createDriver() throws MalformedURLException {
+
+    }
+}
